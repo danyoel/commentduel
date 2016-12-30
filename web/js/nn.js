31e755b8abbfaff7fb99cb642e87fd8d3c4f5e45
danl <dan@liebling.org>
2016-12-29 16:39:05 -0800

    add simple Angular mockup

---
+++ web/js/nn.js
@@ -0,0 +1,40 @@
+﻿/* Copyright 2017 Daniel J. Liebling. All rights reserved. */
+
+
+var nnApp = angular.module('nnApp', []);
+
+nnApp.controller('MainController', ['$scope', 'comments', function ($scope, comments) {
+    $scope.submit = function () {
+        comments.postRebuttal();
+    };
+
+    $scope.nextComment = function () {
+        comments.getComment();
+    };
+
+    $scope.comment = {
+        id: 123456,
+        projectId: 54321,
+        text: "Loading comment..."
+    };
+}]);
+
+
+nnApp.factory('comments', function () {
+
+    var getComment = function () {
+        return {
+            "id": 1247532,
+            "text": "Lorem ipsum i need moar parking directly in front of my house"
+        };
+    };
+
+    var postRebuttal = function (inResponseTo, onBehalfOf, rebuttal) {
+
+    };
+
+    return {
+        "getComment": getComment,
+        "postRebuttal": postRebuttal 
+    };
+});
